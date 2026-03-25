6b1edab81f5ab176d8c1493490dcd0e6b6dd90de
Frankie <1426203851@qq.com>
2026-03-25 20:18:00 +0800

    chore: update deps

--- eslint.config.js
+++ eslint.config.js
@@ -4,7 +4,6 @@ export default defineConfig({
   ignores: ['archives/**', 'docs/**', 'images/**'],
   typescript: true,
   rules: {
-    'e18e/prefer-static-regex': 'off',
     'markdown/require-alt-text': 'off',
   },
 })
