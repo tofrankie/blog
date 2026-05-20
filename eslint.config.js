6e94d46cd16f235d19cc2f88b19c68715345c6d7
Frankie <1426203851@qq.com>
2026-05-21 03:45:46 +0800

    chore: update deps

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,4 @@ import { defineConfig } from '@tofrankie/eslint'
 export default defineConfig({
   ignores: ['archives/**', 'docs/**', 'images/**'],
   typescript: true,
-  rules: {
-    'markdown/require-alt-text': 'off',
-  },
 })
